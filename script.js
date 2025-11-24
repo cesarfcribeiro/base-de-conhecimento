d7048971acbd01d12f671fa4c1fc6026f45c63f7
cesarfcribeiro <cesarfcribeiro@gmail.com>
2025-11-24 09:21:20 -0300

    funcionalidades js

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
+let cardContainer = document.querySelector(".card-container");
+let campoBusca = document.querySelector("header input");
 let dados = [];
 
 async function iniciarBusca() {
-    let resposta = await fetch("data.json");
-    dados = await resposta.json();
-    console.log(dados);
+  if (dados.length === 0) {
+    try {
+      let resposta = await fetch("data.json");
+      dados = await resposta.json();
+    } catch (error) {
+      console.error("Falha ao buscar dados:", error);
+      return; // Interrompe a execução se houver erro
+    }
+  }
+
+  const termoBusca = campoBusca.value.toLowerCase();
+  const dadosFiltrados = dados.filter((dado) => dado.nome.toLowerCase().includes(termoBusca) || dado.descrição.toLowerCase().includes(termoBusca));
+
+  renderizarCards(dadosFiltrados);
+}
+
+function renderizarCards(dados) {
+  cardContainer.innerHTML = ""; // Limpa os cards existentes antes de renderizar novos
+  for (let dado of dados) {
+    let article = document.createElement("article");
+    article.classList.add("card");
+    article.innerHTML = `
+        <h2>${dado.nome}</h2>
+        <p>${dado.ano}</p>
+        <p>${dado.descrição}</p>
+        <a href="${dado.link}" target="_blank">Saiba mais</a>
+        `;
+    cardContainer.appendChild(article);
+  }
 }
